458846dd5b056d02af326808e678b361ca0b570d
developer <1735721264@qq.com>
2024-06-03 22:19:02 +0800

    x

--- src/Utils.Layout.js
+++ src/Utils.Layout.js
@@ -1,49 +1,104 @@
-const rowleft = (position, positions) => {
+const horizontal = (position, positions) => {
   var x = 0
 
   var l = []
 
   positions.forEach(i => {
     if (x + i.w < position || x + i.w === position) {
-      rc.push(Object({ ...i, x: position.x + t.x, y: position.y }))
+      l.push(Object({ ...i, x: position.x + x, y: position.y }))
       x = x + i.w
     }
   })
 
-  return { position: l, rest: position.filter((i, index) => index > l.length - 1) }
+  return { result: l, rest: position.filter((i, index) => index > l.length - 1) }
 }
 
-const rowright = (position, positions) => {
+const horizontalreverse = (position, positions) => {
   var x = 0
 
   var l = []
 
   positions.forEach(i => {
     if (x + i.w < position || x + i.w === position) {
-      rc.push(Object({ ...i, x: position.x + position.w - t.x, y: position.y }))
+      l.push(Object({ ...i, x: position.x + position.w - x, y: position.y }))
       x = x + i.w
     }
   })
 
-  return { position: rl, rest: position.filter((i, index) => index > rl.length - 1) }
+  return { result: l, rest: position.filter((i, index) => index > l.length - 1) }
 }
 
-
-const rowcenter = (position, positions) => {
+const horizontalcenter = (position, positions) => {
   var x = 0
 
   var l = []
 
   positions.forEach(i => {
     if (x + i.w < position || x + i.w === position) {
-      rc.push(Object({ ...i, x: position.x + position.w - t.x, y: position.y }))
+      rc.push(Object({ ...i }))
       x = x + i.w
     }
   })
 
-  return { position: rl, rest: position.filter((i, index) => index > rl.length - 1) }
+  var tw = l.reduce((t, i) => t + i.w, 0)
+
+  var tx = 0
+
+  l.forEach(i => {
+    i.x = position.x + (position.w - tw) / 2 + tx
+    tx = tx + i.w
+  })
+
+  return { result: l, rest: position.filter((i, index) => index > l.length - 1) }
 }
 
+const horizontalaround = (position, positions) => {
+  var x = 0
+
+  var l = []
+
+  positions.forEach(i => {
+    if (x + i.w < position || x + i.w === position) {
+      rc.push(Object({ ...i }))
+      x = x + i.w
+    }
+  })
+
+  var tw = l.reduce((t, i) => t + i.w, 0)
+
+  var tx = 0
+
+  l.forEach((i, index) => {
+    i.x = position.x + (position.w - tw) / (l.length - 1) * index + tx
+    tx = tx + i.w
+  })
+
+  return { result: l, rest: position.filter((i, index) => index > l.length - 1) }
+}
+
+const horizontalbetween = (position, positions) => {
+  var x = 0
+
+  var l = []
+
+  positions.forEach(i => {
+    if (x + i.w < position || x + i.w === position) {
+      rc.push(Object({ ...i }))
+      x = x + i.w
+    }
+  })
+
+  var tw = l.reduce((t, i) => t + i.w, 0)
+
+  var tx = 0
+
+  l.forEach((i, index) => {
+    i.x = position.x + (position.w - tw) / (l.length + 1) * (index + 1) + tx
+    tx = tx + i.w
+  })
+
+  return { result: l, rest: position.filter((i, index) => index > l.length - 1) }
+}
 
 const gridrow = (position, positions) => {
   var rx = 0
@@ -71,6 +126,6 @@ const gridrow = (position, positions) => {
   return rl
 }
 
-const Layout = { add, center, centered, pointcover, coordinate, min, max }
+const Layout = { horizontal, horizontalreverse, horizontalcenter, horizontalaround, horizontalbetween }
 
 export default Layout
