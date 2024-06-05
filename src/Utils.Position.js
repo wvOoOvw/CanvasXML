0294f68c7af63df06fb182e7c7c7dde9d8c530ba
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-06-05 17:11:31 +0800

    x

--- src/Utils.Position.js
+++ src/Utils.Position.js
@@ -1,3 +1,9 @@
+const _position = ['x', 'y', 'w', 'h']
+
+const _wireframe = ['l', 'r', 't', 'b']
+
+const _coordinate = ['x', 'y', 'w', 'h','l', 'r', 't', 'b', 'vmax', 'vw', 'vh', 'cx', 'cy']
+
 const add = (positions) => positions.reduce((t, i) => Object({ x: t.x + (i.x || 0), y: t.y + (i.y || 0), w: t.w + (i.w || 0), h: t.h + (i.h || 0) }), { x: 0, y: 0, w: 0, h: 0 })
 
 const box = (positions) => positions.reduce((t, i) => Object({ x: i.x ? Math.min(t.x, i.x) : t.x, y: i.y ? Math.min(t.y, i.y) : t.y, w: i.x && i.w ? Math.max(t.w, i.x + i.w) : t.w, h: i.y && i.h ? Math.max(t.h, i.y + i.h) : t.h }), { x: 0, y: 0, w: 0, h: 0 })
@@ -10,6 +16,7 @@ const hmin = (positions) => positions.reduce((t, i) => i.h ? Math.min(i.h, t) : 
 
 const hmax = (positions) => positions.reduce((t, i) => i.h ? Math.max(i.h, t) : t, 0)
 
+
 const vmin = (position) => Math.min(position.w, position.h) * 0.01
 
 const vmax = (position) => Math.max(position.w, position.h) * 0.01
@@ -18,26 +25,25 @@ const vw = (position) => position.w * 0.01
 
 const vh = (position) => position.h * 0.01
 
-const cx = (position) => position.x + position.w / 2
+const centerx = (position) => position.x + position.w / 2
 
-const cy = (position) => position.y + position.h / 2
+const centery = (position) => position.y + position.h / 2
+
+const centerreversex = (position) => position.x - position.w / 2
+
+const centerreversey = (position) => position.y - position.h / 2
+
 
 const center = (position) => Object({ x: (position.x || 0) + (position.w ? position.w / 2 : 0), y: (position.y || 0) + (position.h ? position.h / 2 : 0), w: (position.w || 0), h: (position.h || 0) })
 
-const centerx = (position) => Object({ x: (position.x || 0) + (position.w ? position.w / 2 : 0), y: (position.y || 0), w: (position.w || 0), h: (position.h || 0) })
+const centerreverse = (position) => Object({ x: (position.x || 0) - (position.w ? position.w / 2 : 0), y: (position.y || 0) - (position.h ? position.h / 2 : 0), w: (position.w || 0), h: (position.h || 0) })
 
-const centery = (position) => Object({ x: (position.x || 0), y: (position.y || 0) + (position.h ? position.h / 2 : 0), w: (position.w || 0), h: (position.h || 0) })
 
-const centered = (position) => Object({ x: (position.x || 0) - (position.w ? position.w / 2 : 0), y: (position.y || 0) - (position.h ? position.h / 2 : 0), w: (position.w || 0), h: (position.h || 0) })
-
-const centeredx = (position) => Object({ x: (position.x || 0) - (position.w ? position.w / 2 : 0), y: (position.y || 0), w: (position.w || 0), h: (position.h || 0) })
-
-const centeredy = (position) => Object({ x: (position.x || 0), y: (position.y || 0) - (position.h ? position.h / 2 : 0), w: (position.w || 0), h: (position.h || 0) })
-
-const coordinate = (position) => new Array([[vmax, 'vmax'], [vw, 'vw'], [vh, 'vh'], [cx, 'cx'], [cy, 'cy'], [border]]).reduce((t, i) => Object.assign(t, { [i[1]]: i[0](position) } ), Object({ x: position.x, y: position.y, w: position.w, h: position.h, l: position.x, r: position.x + position.w, t: position.y, b: position.y + position.h }))
+const coordinate = (position) => new Array([[vmax, 'vmax'], [vw, 'vw'], [vh, 'vh'], [centerx, 'cx'], [centery, 'cy'], [border]]).reduce((t, i) => Object.assign(t, { [i[1]]: i[0](position) } ), Object({ x: position.x, y: position.y, w: position.w, h: position.h, l: position.x, r: position.x + position.w, t: position.y, b: position.y + position.h }))
 
 const pointcover = (position, point) => point.x >= position.x && point.x <= position.x + position.w && point.y >= position.y && point.y <= position.y + position.h
 
-const Position = { add, box, wmin, wmax, hmin, hmax, vmin, vmax, vw, vh, cx, cy, center, centerx, centery, centered,centeredx, centeredy, coordinate, pointcover }
+
+const Position = { add, box, wmin, wmax, hmin, hmax, vmin, vmax, vw, vh, center, centerx, centery, centerreverse, coordinate, pointcover }
 
 export default Position
