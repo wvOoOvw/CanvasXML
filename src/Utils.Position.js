f09654a62946291df38112eb32b6ade147f92b27
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-06-05 17:38:04 +0800

    x

--- src/Utils.Position.js
+++ src/Utils.Position.js
@@ -2,20 +2,11 @@ const _position = ['x', 'y', 'w', 'h']
 
 const _wireframe = ['l', 'r', 't', 'b']
 
-const _coordinate = ['x', 'y', 'w', 'h','l', 'r', 't', 'b', 'vmax', 'vw', 'vh', 'cx', 'cy']
+const _point = ['cx', 'cy', 'tlx', 'tly', 'trx', 'try', 'blx', 'bly', 'brx', 'bry']
 
-const add = (positions) => positions.reduce((t, i) => Object({ x: t.x + (i.x || 0), y: t.y + (i.y || 0), w: t.w + (i.w || 0), h: t.h + (i.h || 0) }), { x: 0, y: 0, w: 0, h: 0 })
+const _viewport = ['vmin', 'vmax', 'vw', 'vh']
 
-const box = (positions) => positions.reduce((t, i) => Object({ x: i.x ? Math.min(t.x, i.x) : t.x, y: i.y ? Math.min(t.y, i.y) : t.y, w: i.x && i.w ? Math.max(t.w, i.x + i.w) : t.w, h: i.y && i.h ? Math.max(t.h, i.y + i.h) : t.h }), { x: 0, y: 0, w: 0, h: 0 })
-
-const wmin = (positions) => positions.reduce((t, i) => i.w ? Math.min(i.w, t) : t, 0)
-
-const wmax = (positions) => positions.reduce((t, i) => i.w ? Math.max(i.w, t) : t, 0)
-
-const hmin = (positions) => positions.reduce((t, i) => i.h ? Math.min(i.h, t) : t, 0)
-
-const hmax = (positions) => positions.reduce((t, i) => i.h ? Math.max(i.h, t) : t, 0)
-
+const _coordinate = _position & _wireframe & _point & _viewport
 
 const vmin = (position) => Math.min(position.w, position.h) * 0.01
 
