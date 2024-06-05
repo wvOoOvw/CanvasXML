63b99ace66bf33cb2e58269036161f80289a270d
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-06-05 15:54:50 +0800

    x

--- src/Utils.Position.js
+++ src/Utils.Position.js
@@ -10,6 +10,18 @@ const hmin = (positions) => positions.reduce((t, i) => i.h ? Math.min(i.h, t) : 
 
 const hmax = (positions) => positions.reduce((t, i) => i.h ? Math.max(i.h, t) : t, 0)
 
+const vmin = (position) => Math.min(position.w, position.h) * 0.01
+
+const vmax = (position) => Math.max(position.w, position.h) * 0.01
+
+const vw = (position) => position.w * 0.01
+
+const vh = (position) => position.h * 0.01
+
+const cx = (position) => position.x + position.w / 2
+
+const cy = (position) => position.y + position.h / 2
+
 const center = (position) => Object({ x: (position.x || 0) + (position.w ? position.w / 2 : 0), y: (position.y || 0) + (position.h ? position.h / 2 : 0), w: (position.w || 0), h: (position.h || 0) })
 
 const centerx = (position) => Object({ x: (position.x || 0) + (position.w ? position.w / 2 : 0), y: (position.y || 0), w: (position.w || 0), h: (position.h || 0) })
@@ -22,18 +34,10 @@ const centeredx = (position) => Object({ x: (position.x || 0) - (position.w ? po
 
 const centeredy = (position) => Object({ x: (position.x || 0), y: (position.y || 0) - (position.h ? position.h / 2 : 0), w: (position.w || 0), h: (position.h || 0) })
 
+const coordinate = (position) => new Array([[vmax, 'vmax'], [vw, 'vw'], [vh, 'vh'], [cx, 'cx'], [cy, 'cy'], [border]]).reduce((t, i) => Object.assign(t, { [i[1]]: i[0](position) } ), Object({ x: position.x, y: position.y, w: position.w, h: position.h, l: position.x, r: position.x + position.w, t: position.y, b: position.y + position.h }))
+
 const pointcover = (position, point) => point.x >= position.x && point.x <= position.x + position.w && point.y >= position.y && point.y <= position.y + position.h
 
-const coordinate = (position) => Object({ x: position.x + position.w / 2, y: position.y + position.h / 2, w: position.w, h: position.h, l: position.x, r: position.x + position.w, t: position.y, b: position.y + position.h })
-
-const vmin = (position) => Math.min(position.w, position.h) * 0.01
-
-const vmax = (position) => Math.max(position.w, position.h) * 0.01
-
-const vw = (position) => position.w * 0.01
-
-const vh = (position) => position.h * 0.01
-
-const Position = { add, box, wmin, wmax, hmin, hmax, center,centerx, centery, centered,centeredx, centeredy, pointcover, coordinate, vmin, vmax, vw, vh }
+const Position = { add, box, wmin, wmax, hmin, hmax, vmin, vmax, vw, vh, cx, cy, center, centerx, centery, centered,centeredx, centeredy, coordinate, pointcover }
 
 export default Position
