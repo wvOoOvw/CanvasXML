2d970876fa44db3a5142e6984bf7d123b88aa495
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-06-13 18:00:26 +0800

    x

--- example/Text/App.js
+++ example/Text/App.js
@@ -11,15 +11,23 @@ function App() {
       <layout w={coordinate.vw * 45} h={coordinate.vh * 25} verticalAlignCenter horizontalAlignCenter>
         <layout w={coordinate.vw * 45} h={coordinate.vh * 25}>
           {
-            (props) => <text isolated fillText font='20px monospace' fillStyle='white' text={'第五人格职业联赛，简称IVL（Identity V League）。是中国大陆地区最高级别的第五人格职业比赛，是每年中国大陆赛区通往第五人格深渊的呼唤COA（Call of the Abyss）全球赛的重要通道之一。同时，IVL还是国内首个非对称对抗竞技职业联赛。'} gap={12} {...props} />
+            (props) => <rect {...props}>
+              <clip>
+                <text isolated fillText font='20px monospace' fillStyle='white' text={'第五人格职业联赛，简称IVL（Identity V League）。是中国大陆地区最高级别的第五人格职业比赛，是每年中国大陆赛区通往第五人格深渊的呼唤COA（Call of the Abyss）全球赛的重要通道之一。同时，IVL还是国内首个非对称对抗竞技职业联赛。'} gap={12} {...props} />
+              </clip>
+            </rect>
           }
         </layout>
       </layout>
 
       <layout w={coordinate.vw * 30} h={coordinate.vh * 20}  verticalAlignCenter horizontalAlignCenter>
-        <layout w={coordinate.vw * 30} h={coordinate.vh * 20} >
-          {
-            (props) => <text isolated fillText font='20px monospace' fillStyle='white' text={'第五人格职业联赛，简称IVL（Identity V League）。是中国大陆地区最高级别的第五人格职业比赛，是每年中国大陆赛区通往第五人格深渊的呼唤COA（Call of the Abyss）全球赛的重要通道之一。同时，IVL还是国内首个非对称对抗竞技职业联赛。'} gap={12} {...props} />
+        <layout w={coordinate.vw * 30} h={coordinate.vh * 20}>
+        {
+            (props) => <rect {...props}>
+              <clip>
+                <text isolated fillText font='20px monospace' fillStyle='white' text={'第五人格职业联赛，简称IVL（Identity V League）。是中国大陆地区最高级别的第五人格职业比赛，是每年中国大陆赛区通往第五人格深渊的呼唤COA（Call of the Abyss）全球赛的重要通道之一。同时，IVL还是国内首个非对称对抗竞技职业联赛。'} gap={12} {...props} />
+              </clip>
+            </rect>
           }
         </layout>
       </layout>
