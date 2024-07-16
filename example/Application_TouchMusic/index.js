5fe444f908640f09f89ff650e0838fbd4ac459d1
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-07-16 16:57:23 +0800

    x

--- example/Application_TouchMusic/index.js
+++ example/Application_TouchMusic/index.js
@@ -12,6 +12,7 @@ canvas.style.width = '100%'
 canvas.style.height = '100%'
 canvas.style.background = 'black'
 canvas.style.overflow = 'hidden'
+canvas.style.userSelect = 'none'
 window.addEventListener('wheel', e => e.preventDefault(), { passive: false })
 window.addEventListener('touchmove', e => e.preventDefault(), { passive: false })
 window.addEventListener('contextmenu', e => e.preventDefault(), { passive: false })
