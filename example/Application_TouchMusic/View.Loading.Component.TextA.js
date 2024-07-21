06b84ce47ba9127291e9295b971a2fc49a813506
developer <1735721264@qq.com>
2024-07-21 22:39:20 +0800

    x

--- example/Application_TouchMusic/View.Loading.Component.TextA.js
+++ example/Application_TouchMusic/View.Loading.Component.TextA.js
@@ -5,7 +5,7 @@ import ContextApp from './Context.App'
 function App() {
   const contextApp = React.useContext(ContextApp)
 
-  return <ReactCanvas2d.TextCaculateLine text={`PHIGROS`} font={`bolder ${contextApp.unitpx * 0.12}px sans-serif`} lineHeight={1} gap={0} w={contextApp.locationLayout.w - contextApp.unitpx * 0.08} split=' ' wrap>
+  return <ReactCanvas2d.TextCaculateLine text={`WIRELOST`} font={`bolder ${contextApp.unitpx * 0.12}px sans-serif`} lineHeight={1} gap={0} w={contextApp.locationLayout.w - contextApp.unitpx * 0.08} split=' ' wrap>
     {
       (line, location) => {
         return <layout w={location.w} h={location.h} item>
