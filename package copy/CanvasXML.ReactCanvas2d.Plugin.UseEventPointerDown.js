a4d4223df5154cd713ca92d83d82d28224251d84
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-08-06 15:56:26 +0800

    x

--- package copy/CanvasXML.ReactCanvas2d.Plugin.UseEventPointerDown.js
+++
@@ -1,8 +0,0 @@
-import React from './CanvasXML.React'
-import Canvas2d from './CanvasXML.Canvas2d'
-
-const useEventPointerDown = (props) => {
-  return { onTouchStart: props.onPointerDown, onMouseDown: props.onPointerDown }
-}
-
-export default useEventPointerDown
