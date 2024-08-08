b91a13a87c0b01012ed6f39761c7a12a72c5d46e
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-08-08 10:44:30 +0800

    x

--- package/index.js
+++ package/index.js
@@ -1,7 +1,7 @@
 import React from './React'
 import Canvas2d from './Canvas2d'
 import ReactCanvas2d from './ReactCanvas2d'
-import ReactExtensions from './ReactExtensions'
-import ReactCanvas2dExtensions from './ReactCanvas2dExtensions'
+import * as ReactExtensions from './ReactExtensions'
+import * as ReactCanvas2dExtensions from './ReactCanvas2dExtensions'
 
 export { React, Canvas2d, ReactCanvas2d, ReactExtensions, ReactCanvas2dExtensions }
