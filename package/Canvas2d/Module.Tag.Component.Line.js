571641214744b9cbfd0ce7b7211013259b04e920
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-08-19 19:46:40 +0800

    x

--- package/Canvas2d/Module.Tag.Component.Line.js
+++ package/Canvas2d/Module.Tag.Component.Line.js
@@ -1,15 +1,20 @@
 import Core from './Core'
 
 const App = {
+  onLocationUnmounted: (dom) => {
+    if (dom.path !== undefined) {
+      dom._path = dom.path
+    }
+    if (dom.path === undefined){
+      dom._path = dom.children.filter((i) => i.element.tag === 'path').map((i) => i.props)
+    }
+  },
   onRenderMounting: (dom) => {
-    dom.children
-      .filter((i) => i.element.tag === 'path')
-      .map((i) => i.props)
-      .forEach((i, index) => {
-        if (index === 0) Core.context().moveTo(i.x, i.y)
-        if (index === 0) Core.context().lineTo(i.x, i.y)
-        if (index !== 0) Core.context().lineTo(i.x, i.y)
-      })
+    dom._path.forEach((i, index) => {
+      if (index === 0) Core.context().moveTo(i.x, i.y)
+      if (index === 0) Core.context().lineTo(i.x, i.y)
+      if (index !== 0) Core.context().lineTo(i.x, i.y)
+    })
   },
 }
 
