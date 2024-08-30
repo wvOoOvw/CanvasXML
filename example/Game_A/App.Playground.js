a05b042db68bd585e7e078531c75379be2d58583
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-08-30 16:44:26 +0800

    x

--- example/Game_A/App.Playground.js
+++ example/Game_A/App.Playground.js
@@ -11,7 +11,6 @@ import Animation from './App.Playground.Animation'
 import Background from './App.Playground.Background'
 import Enemy from './App.Playground.Enemy'
 import Infomation from './App.Playground.Infomation'
-import Map from './App.Playground.Map'
 import Setting from './App.Playground.Setting'
 import Role from './App.Playground.Role'
 
@@ -20,46 +19,25 @@ import { jsonA } from './json'
 function App() {
   const contextApp = React.useContext(ContextApp)
 
-  const [gameLoadMap, setGameLoadMap] = React.useState(false)
   const [gameLoadEnemy, setGameLoadEnemy] = React.useState(false)
   const [gameLoadRole, setGameLoadRole] = React.useState(false)
   const [gameAnimation, setGameAnimation] = React.useState([])
   const [gamePlay, setGamePlay] = React.useState(true)
   const [gameTimeRate, setGameTimeRate] = React.useState(1)
-  const [gameMap, setGameMap] = React.useState([])
   const [gameEnemy, setGameEnemy] = React.useState([])
   const [gameEnemyReady, setGameEnemyReady] = React.useState([])
   const [gameRole, setGameRole] = React.useState([])
   const [gameRoleActive, setGameRoleActive] = React.useState()
 
-  const load = gameLoadMap && gameLoadEnemy && gameLoadRole
+  const load = gameLoadEnemy && gameLoadRole
 
   const { animationCount: animationCountGameTime } = ReactExtensions.useAnimationDestination({ play: gamePlay && load, defaultCount: 0, destination: Infinity, rate: gameTimeRate, postprocess: n => Number(n.toFixed(4)) })
 
   const informationJson = React.useMemo(() => jsonA(contextApp), [])
 
-  const unitpx = React.useMemo(() => {
-    var r = 0
-
-    gameMap.forEach(i => {
-      const containerW = contextApp.locationLayout.w * 0.8
-      const containerH = contextApp.locationLayout.h * 0.8
-    
-      const dimensionW = containerW / i.dimension[0]
-      const dimensionH = containerH / i.dimension[1]
-    
-      const dimensionMin = Math.min(dimensionW, dimensionH)
-
-      r = Math.min(r, dimensionMin)
-    })
-
-    return r
-  }, [gameMap])
-
   const zIndex = React.useMemo(() => {
     const positive = new Array(
       'RoleBackground',
-      'MapMeth',
       'EnemyMeth',
       'RoleMeth',
       'RoleAnimation',
@@ -75,13 +53,12 @@ function App() {
     return { ...positive, ...negative }
   }, [])
 
-  return <ContextPlayground.Provider value={{ gameLoadMap, setGameLoadMap, gameLoadEnemy, setGameLoadEnemy, gameLoadRole, setGameLoadRole, gameAnimation, setGameAnimation, gamePlay, setGamePlay, gameTimeRate, setGameTimeRate, gameMap, setGameMap, gameEnemy, setGameEnemy, gameEnemyReady, setGameEnemyReady, gameRole, setGameRole, gameRoleActive, setGameRoleActive, load, animationCountGameTime, informationJson, unitpx, zIndex }}>
+  return <ContextPlayground.Provider value={{ gameLoadMap, setGameLoadMap, gameLoadEnemy, setGameLoadEnemy, gameLoadRole, setGameLoadRole, gameAnimation, setGameAnimation, gamePlay, setGamePlay, gameTimeRate, setGameTimeRate, gameEnemy, setGameEnemy, gameEnemyReady, setGameEnemyReady, gameRole, setGameRole, gameRoleActive, setGameRoleActive, load, animationCountGameTime, informationJson, unitpx, zIndex }}>
     <layout>
       <Animation />
       <Background />
       <Enemy />
       <Infomation />
-      <Map />
       <Setting />
       <Role />
     </layout>
