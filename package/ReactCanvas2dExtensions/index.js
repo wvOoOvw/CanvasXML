639f60af45dab39942a534cc54702cec6ccf3ca9
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-09-02 15:39:01 +0800

    x

--- package/ReactCanvas2dExtensions/index.js
+++ package/ReactCanvas2dExtensions/index.js
@@ -9,14 +9,11 @@ import Text from './Component.Text'
 
 import useLoadAudio from './Hook.useLoadAudio'
 import useEventClick from './Hook.UseEventClick'
-import useEventCompose from './Hook.UseEventCompose'
 import useEventDragControl from './Hook.UseEventDragControl'
 import useLoadImage from './Hook.useLoadImage'
-import useLocationBox from './Hook.UseLocationBox'
 import useLocationProperty from './Hook.UseLocationProperty'
-import useResourceReload from './Hook.UseResourceReload'
 
 import flatDom from './Utils.FlatDom'
 import getDomById from './Utils.GetDomById'
 
-export { Accordion, Button, CanvasLayout, CoordinateHelper, PoweredBy, Rotate, RotateCenter, Text, useLoadAudio, useEventClick, useEventCompose, useEventDragControl, useLoadImage, useLocationBox, useLocationProperty, useResourceReload, flatDom, getDomById }
+export { Accordion, Button, CanvasLayout, CoordinateHelper, PoweredBy, Rotate, RotateCenter, Text, useLoadAudio, useEventClick, useEventDragControl, useLoadImage, useLocationProperty, flatDom, getDomById }
