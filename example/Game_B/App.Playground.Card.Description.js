242cd82cf122d204d5893881753391b12b91ce08
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-09-05 18:10:30 +0800

    x

--- example/Game_B/App.Playground.Card.Description.js
+++ example/Game_B/App.Playground.Card.Description.js
@@ -38,35 +38,79 @@ function Template(props) {
     },
   ]
 
-  return <layout x={x} y={y} w={w} h={h} transform={transform}>
+  return <>
+    <layout x={x} y={y} w={w} h={h} transform={transform}>
 
-    <rectradius fill fillStyle='rgb(75, 75, 75)' radius={min * 0.048} shadowBlur={min * 0.08} shadowColor='rgb(255, 255, 255)' globalAlpha={animationCountAppear * 0.8} onPointerDown={onPointerDown} onPointerMove={onPointerMove} onPointerMoveAway={onPointerMove} onPointerUp={onPointerUp} onPointerUpAway={onPointerUp} />
+<rectradius fill fillStyle='rgb(75, 75, 75)' radius={min * 0.048} shadowBlur={min * 0.08} shadowColor='rgb(255, 255, 255)' globalAlpha={animationCountAppear * 0.8} onPointerDown={onPointerDown} onPointerMove={onPointerMove} onPointerMoveAway={onPointerMove} onPointerUp={onPointerUp} onPointerUpAway={onPointerUp} />
 
-    <rectradius cx='50%' cy='50%' w={w - min * 0.04} h={h - min * 0.04} clip radius={min * 0.048}>
-      <image cx='50%' cy='50%' src={contextApp[card.imageIndex]} clipHorizontalCenter clipVerticalCenter globalAlpha={animationCountAppear * 0.8}/>
-    </rectradius>
+<rectradius cx='50%' cy='50%' w={w - min * 0.04} h={h - min * 0.04} clip radius={min * 0.048}>
+  <image cx='50%' cy='50%' src={contextApp[card.imageIndex]} clipHorizontalCenter clipVerticalCenter globalAlpha={animationCountAppear * 0.8}/>
+</rectradius>
 
-    <layout cx={min * 0.2} cy={min * 0.2} w={min * 0.24} h={min * 0.24}>
-      <rectradius fill fillStyle='rgb(75, 75, 75)' radius={min * 0.048} globalAlpha={animationCountAppear * 0.8}/>
-      {
-        card.modelType === 'Role' ? <image cx='50%' cy='50%' w='75%' h='75%' src={contextApp.imagePngDigitalTraceWhite} /> : null
+<layout cx={min * 0.2} cy={min * 0.2} w={min * 0.24} h={min * 0.24}>
+  <rectradius fill fillStyle='rgb(75, 75, 75)' radius={min * 0.048} globalAlpha={animationCountAppear * 0.8}/>
+  {
+    card.modelType === 'Role' ? <image cx='50%' cy='50%' w='75%' h='75%' src={contextApp.imagePngDigitalTraceWhite} /> : null
+  }
+</layout>
+
+<layout cx='50%' cy={`calc(100% - ${min * 0.2}px)`} w={w - min * 0.24} h={min * 0.2}>
+  <rectradius fill fillStyle='rgb(75, 75, 75)' radius={min * 0.032}  globalAlpha={animationCountAppear * 0.8} />
+  <ReactCanvas2dExtensions.Text text={card.descriptionName} font={`bolder ${min * 0.08}px sans-serif`} w={Infinity}>
+    {
+      (line, location) => {
+        return line.map(i => {
+          return <text cx='50%' cy='50%' w={i.w} h={i.h} fillText fillStyle='rgb(255, 255, 255)' text={i.text} font={i.font} globalAlpha={animationCountAppear}/>
+        })
       }
-    </layout>
+    }
+  </ReactCanvas2dExtensions.Text>
+</layout>
 
-    <layout cx='50%' cy={`calc(100% - ${min * 0.2}px)`} w={w - min * 0.24} h={min * 0.2}>
-      <rectradius fill fillStyle='rgb(75, 75, 75)' radius={min * 0.032}  globalAlpha={animationCountAppear * 0.8} />
-      <ReactCanvas2dExtensions.Text text={card.descriptionName} font={`bolder ${min * 0.08}px sans-serif`} w={Infinity}>
-        {
-          (line, location) => {
-            return line.map(i => {
-              return <text cx='50%' cy='50%' w={i.w} h={i.h} fillText fillStyle='rgb(255, 255, 255)' text={i.text} font={i.font} globalAlpha={animationCountAppear}/>
-            })
-          }
-        }
-      </ReactCanvas2dExtensions.Text>
-    </layout>
+</layout>
+  
+    <layout x={x} y={y} w={w} h={h} transform={transform}>
 
-  </layout>
+    <layout cx={w + (w - min * 0.24) / 2 + min * 0.12} cy={`calc(100% - ${min * 0.2}px)`} w={w - min * 0.24} h={min * 0.2}>
+            <rectradius fill fillStyle={color} radius={min * 0.032} shadowBlur={min * 0.08} shadowColor='rgb(255, 255, 255)' globalAlpha={animationCountAppear * 0.8}/>
+            <ReactCanvas2dExtensions.Text text={'ATTACT  765'} font={`bolder ${min * 0.08}px sans-serif`} w={Infinity}>
+              {
+                (line, location) => {
+                  return line.map(i => {
+                    return <text cx='50%' cy='50%' w={i.w} h={i.h} fillText fillStyle='rgb(255, 255, 255)' text={i.text} font={i.font} globalAlpha={animationCountAppear} />
+                  })
+                }
+              }
+            </ReactCanvas2dExtensions.Text>
+          </layout>
+
+          <layout cx={w + (w - min * 0.24) / 2 + min * 0.12} cy={`calc(100% - ${min * 0.5}px)`} w={w - min * 0.24} h={min * 0.2}>
+            <rectradius fill fillStyle={color} radius={min * 0.032} shadowBlur={min * 0.08} shadowColor='rgb(255, 255, 255)' globalAlpha={animationCountAppear * 0.8}/>
+            <ReactCanvas2dExtensions.Text text={'ATTACT  765'} font={`bolder ${min * 0.08}px sans-serif`} w={Infinity}>
+              {
+                (line, location) => {
+                  return line.map(i => {
+                    return <text cx='50%' cy='50%' w={i.w} h={i.h} fillText fillStyle='rgb(255, 255, 255)' text={i.text} font={i.font} globalAlpha={animationCountAppear} />
+                  })
+                }
+              }
+            </ReactCanvas2dExtensions.Text>
+          </layout>
+
+          <layout cx={w + (w - min * 0.24) / 2 + min * 0.12} cy={`calc(100% - ${min * 0.8}px)`} w={w - min * 0.24} h={min * 0.2}>
+            <rectradius fill fillStyle={color} radius={min * 0.032} shadowBlur={min * 0.08} shadowColor='rgb(255, 255, 255)' globalAlpha={animationCountAppear * 0.8}/>
+            <ReactCanvas2dExtensions.Text text={'ATTACT  765'} font={`bolder ${min * 0.08}px sans-serif`} w={Infinity}>
+              {
+                (line, location) => {
+                  return line.map(i => {
+                    return <text cx='50%' cy='50%' w={i.w} h={i.h} fillText fillStyle='rgb(255, 255, 255)' text={i.text} font={i.font} globalAlpha={animationCountAppear} />
+                  })
+                }
+              }
+            </ReactCanvas2dExtensions.Text>
+          </layout>
+</layout>
+  </>
 }
 
 function App() {
