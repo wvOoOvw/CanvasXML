46cb753d015f8d8c050f91b062694192e50ad467
developer <1735721264@qq.com>
2024-09-24 22:21:03 +0800

    x

--- example/Game_B/Model.Card.Magic.Attack.js
+++ example/Game_B/Model.Card.Magic.Attack.js
@@ -7,7 +7,6 @@ const init = (optionOverlay) => {
     {
       descriptionImageIndex: 'imageJpgRoleB',
       descriptionName: '攻击',
-      // descriptionDetail: '25%（50%）[+效果命中：4%',
       descriptionDetail: '回合结束时，召唤暴风雪中的冰晶，形成护甲保护自身。冰甲能够吸收雪女生命上限6%（7.8%）的伤害。技能提升至3（5）级时，当雪女身披冰甲时，敌人对其发动攻击，有25%（50%）[+效果命中：4%（8%）]的概率损失10点速度，持续2回合。',
 
       attributeExpendActionPoint: 1,
