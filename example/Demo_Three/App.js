98ccd116dbd510e8a34f2b4e4d0f036d05f6fcef
developer <1735721264@qq.com>
2024-10-11 22:03:56 +0800

    x

--- example/Demo_Three/App.js
+++ example/Demo_Three/App.js
@@ -69,27 +69,63 @@ function Content() {
   const ObjectOrbitControls = React.useMemo(() => new OrbitControls(contextApp.camera, contextApp.renderer.domElement), [])
   const ObjectAxesHelper = React.useMemo(() => new THREE.AxesHelper(5), [])
 
-  React.useEffectImmediate(() => contextApp.camera.position.set(12, 12, 12), [])
+  React.useEffectImmediate(() => contextApp.camera.position.set(1, 1, 1), [])
   React.useEffectImmediate(() => contextApp.camera.lookAt(0, 0, 0), [])
 
   React.useEffect(() => shouldRender())
 
-  // ReactCanvasThreeJsExtensions.useObjectApply({ apply: true, target: contextApp.scene, object: ObjectAxesHelper })
-
+  ReactCanvasThreeJsExtensions.useObjectApply({ apply: true, target: contextApp.scene, object: ObjectAxesHelper })
   ReactCanvasThreeJsExtensions.useObjectDispose({ object: [ObjectOrbitControls] })
 
   return <>
     <ComponentLight target={contextApp.scene} />
     {
-      new Array(100).fill().map(i =>  <ComponentMeth target={contextApp.scene} />)
+      new Array(100).fill().map(i => <ComponentMeth target={contextApp.scene} />)
     }
-   
+
   </>
 }
 
 function App(props) {
+  const shouldRender = React.useShouldRender()
+
+  const renderer = React.useMemo(() => new THREE.WebGLRenderer({ antialias: true, canvas: props.canvas }), [])
+  const camera = React.useMemo(() => new THREE.PerspectiveCamera(), [])
+  const scene = React.useMemo(() => new THREE.Scene(), [])
+  const raycaster = React.useMemo(() => new THREE.Raycaster(), [])
+
+  const eventListender = React.useMemo(() => ReactCanvasThreeJsExtensions.createEventLinstener({ renderer, camera, scene, raycaster }), [])
+
+  React.useEffect(() => {
+    const rect = props.canvas.getBoundingClientRect()
+
+    rect.x = rect.x
+    rect.y = rect.y
+
+    if (rect.x === undefined) rect.x = rect.left
+    if (rect.y === undefined) rect.y = rect.top
+
+    renderer.setSize(rect.width, rect.height)
+
+    // camera.aspect = rect.width / rect.height
+    // camera.left = 0 - rect.width / 2
+    // camera.right = rect.width / 2
+    // camera.top = rect.height / 2
+    // camera.bottom = 0 - rect.height / 2
+
+    camera.updateProjectionMatrix()
+
+    eventListender.addEventListenerWithCanvas(props.canvas, { dpr: props.dpr, rect })
+
+    return () => eventListender.removeEventListenerWithCanvas(props.canvas)
+  }, [props.canvas, props.dpr])
+
+  React.useEffect(() => shouldRender())
+
+  React.useEffect(() => renderer.render(scene, camera))
+
   const Component =
-    <ContextApp.Provider value={{ ...props }}>
+    <ContextApp.Provider value={{ renderer, camera, scene, raycaster }}>
       <Content />
     </ContextApp.Provider>
 
--- example/Game_C/Model.Weapon.0001.js
+++ example/Game_C/Model.Weapon.0001.js
@@ -17,52 +17,50 @@ function ComponentInWar(props) {
 
   const { animationCount: animationCountAppear } = ReactExtensions.useAnimationDestination({ play: true, defaultCount: 0, destination: inWar ? 1 : 0, rate: 1 / 12, postprocess: n => Number(n.toFixed(4)) })
 
-  const fixed = {
+  const point = {
+    cornerLT: [{ x: 0 - contextApp.unitpx * 0.64, y: 0 - contextApp.unitpx * 0.24 }],
+    cornerRT: [{ x: 0 + contextApp.unitpx * 0.64, y: 0 - contextApp.unitpx * 0.24 }],
+    cornerRB: [{ x: 0 + contextApp.unitpx * 0.64, y: 0 + contextApp.unitpx * 0.24 }],
+    cornerLB: [{ x: 0 - contextApp.unitpx * 0.64, y: 0 + contextApp.unitpx * 0.24 }],
+
+    shelfLT: [{ x: 0 - contextApp.unitpx * 0.72, y: 0 - contextApp.unitpx * 0.48 }],
+    shelfRT: [{ x: 0 + contextApp.unitpx * 0.72, y: 0 - contextApp.unitpx * 0.48 }],
+    shelfRB: [{ x: 0 + contextApp.unitpx * 0.72, y: 0 - contextApp.unitpx * 0.36 }],
+    shelfLB: [{ x: 0 - contextApp.unitpx * 0.72, y: 0 - contextApp.unitpx * 0.36 }],
+
     rivet: [
-      { x: {
-        count: 25,
-        postprocess: n => `${n}`
-      }, y: '25%' },
-      { x: `75%`, y: '25%' },
-      { x: `75%`, y: '75%' },
-      { x: `25%`, y: '75%' },
+      { x: 0 - contextApp.unitpx * 0.64 * 0.5, y: 0 - contextApp.unitpx * 0.24 * 0.5 },
+      { x: 0 + contextApp.unitpx * 0.64 * 0.5, y: 0 - contextApp.unitpx * 0.24 * 0.5 },
+      { x: 0 + contextApp.unitpx * 0.64 * 0.5, y: 0 + contextApp.unitpx * 0.24 * 0.5 },
+      { x: 0 - contextApp.unitpx * 0.64 * 0.5, y: 0 + contextApp.unitpx * 0.24 * 0.5 },
     ],
-    corner: [
-      { x: `0%`, y: '0%' },
-      { x: `100%`, y: '0%' },
-      { x: `100%`, y: '100%' },
-      { x: `0%`, y: '100%' },
-    ],
+
     plank: [
-      { x: `calc(50% + ${contextApp.unitpx * 0.12 * (0 - 3)}px)`, y: `calc(100% - ${contextApp.unitpx * 0.12}px)` },
-      { x: `calc(50% + ${contextApp.unitpx * 0.12 * (0 - 2)}px)`, y: `calc(100% - ${contextApp.unitpx * 0.12}px)` },
-      { x: `calc(50% + ${contextApp.unitpx * 0.12 * (0 - 1)}px)`, y: `calc(100% - ${contextApp.unitpx * 0.12}px)` },
-      { x: `calc(50% + ${contextApp.unitpx * 0.12 * (0)}px)`, y: `calc(100% - ${contextApp.unitpx * 0.12}px)` },
-      { x: `calc(50% + ${contextApp.unitpx * 0.12 * (0 + 1)}px)`, y: `calc(100% - ${contextApp.unitpx * 0.12}px)` },
-      { x: `calc(50% + ${contextApp.unitpx * 0.12 * (0 + 2)}px)`, y: `calc(100% - ${contextApp.unitpx * 0.12}px)` },
-      { x: `calc(50% + ${contextApp.unitpx * 0.12 * (0 + 3)}px)`, y: `calc(100% - ${contextApp.unitpx * 0.12}px)` },
+      { x: contextApp.unitpx * 0.12 * (0 - 3), y: contextApp.unitpx * 0.16 },
+      { x: contextApp.unitpx * 0.12 * (0 - 2), y: contextApp.unitpx * 0.16 },
+      { x: contextApp.unitpx * 0.12 * (0 - 1), y: contextApp.unitpx * 0.16 },
+      { x: contextApp.unitpx * 0.12 * (0), y: contextApp.unitpx * 0.16 },
+      { x: contextApp.unitpx * 0.12 * (0 + 1), y: contextApp.unitpx * 0.16 },
+      { x: contextApp.unitpx * 0.12 * (0 + 2), y: contextApp.unitpx * 0.16 },
+      { x: contextApp.unitpx * 0.12 * (0 + 3), y: contextApp.unitpx * 0.16 },
     ]
   }
-
-  const animationCountFixed = Object.keys(fixed).reduce((a, b) => {
-      return {
-        ...a,
-        [b]: Object.keys(fixed[b]).reduce((c, d) => {
-          return {
-            ...c,
-            [d]: ReactExtensions.useAnimationDestination({ play: true, defaultCount: 0, destination: 1, rate: 1 / 12, postprocess: n => Number(n.toFixed(4)) })
-          }
-        }, {})
-      }
+  const animationCountPoint = Object.keys(point).reduce((r, i) => {
+    return {
+      ...r,
+      [i]: Object.keys(point[i]).reduce((r, n) => {
+        return {
+          ...r,
+          [n]: Object.keys(point[i][n]).reduce((r, v) => {
+            return {
+              ...r,
+              [v]: ReactExtensions.useAnimationDestination({ play: true, defaultCount: point[i][n][v], destination: point[i][n][v], rate: contextApp.unitpx * 0.01, postprocess: n => Number(n.toFixed(4)) })
+            }
+          }, Object())
+        }
+      }, Object())
+    }
   }, Object())
-
-  // const { animationCount: animationCountAppear } = ReactExtensions.useAnimationDestination({ play: true, defaultCount: 0, destination: inWar ? 1 : 0, rate: 1 / 12, postprocess: n => Number(n.toFixed(4)) })
-
-  const onPointerDown = e => {
-  }
-
-  const onPointerUp = e => {
-  }
 
   React.useEffect(() => {
     if (inWar === false && animationCountAppear === 0) {
@@ -73,15 +71,15 @@ function ComponentInWar(props) {
 
   const ComponentRivet = React.useCallback((props) => {
     return <layout cx={props.cx} cy={props.cy} w={contextApp.unitpx * 0.08} h={contextApp.unitpx * 0.08}>
-      <arc fill fillStyle='rgb(175, 175, 175)' cx='50%' cy='50%' radius={contextApp.unitpx * 0.04}/>
+      <arc fill fillStyle='rgb(175, 175, 175)' cx='50%' cy='50%' radius={contextApp.unitpx * 0.04} />
       <rectradiusarc cx='50%' cy='50%' h={contextApp.unitpx * 0.02} fill fillStyle='rgb(255, 255, 255)' radius={contextApp.unitpx * 0.01} />
       <rectradiusarc cx='50%' cy='50%' w={contextApp.unitpx * 0.02} fill fillStyle='rgb(255, 255, 255)' radius={contextApp.unitpx * 0.01} />
     </layout>
-  },[])
+  }, [])
 
   const ComponentPlank = React.useCallback((props) => {
     return <layout cx={props.cx} cy={props.cy} w={contextApp.unitpx * 0.08} h={contextApp.unitpx * 0.24}>
-      <rectradiusarc fill fillStyle='rgb(255, 255, 255)' shadowColor='rgb(255, 255, 255)' radius={contextApp.unitpx * 0.02} shadowBlur={contextApp.unitpx * 0.02} />
+      <rectradiusarc fill fillStyle='rgb(255, 255, 255)' shadowColor='rgb(255, 255, 255)' radius={contextApp.unitpx * 0.02} shadowBlur={contextApp.unitpx * 0.02} onPointerDown={props.onPointerDown} />
       <ReactCanvas2dExtensions.Text text='Do' font={`bolder ${contextApp.unitpx * 0.036}px monospace`} w={Infinity}>
         {
           (line, location) => {
@@ -93,29 +91,63 @@ function ComponentInWar(props) {
   }, [])
 
   const Component =
-    <>
-      <layout cx='50%' cy='50%' w={contextApp.unitpx * 1.2} h={contextApp.unitpx * 0.42} globalAlpha={animationCountAppear}>
-        <rect fill fillStyle='rgb(75, 75, 75)' />
+    <layout cx='50%' cy='50%' w={0} h={0} globalAlpha={animationCountAppear}>
 
-        <path fill fillStyle='rgb(25, 65, 65)' container>
-          <path moveTo>
-            <path x={`calc(0% + ${contextApp.unitpx * 0}px)`} y='0%' />
-          </path>
-          <path lineTo>
-            <path x={`calc(0% + ${contextApp.unitpx * 0}px)`} y='100%' />
-          </path>
-          <path lineTo>
-            <path x={`calc(0% - ${contextApp.unitpx * 0.08}px)`} y={`calc(100% - ${contextApp.unitpx * 0.08}px)`} />
-          </path>
-          <path lineTo>
-            <path x={`calc(0% - ${contextApp.unitpx * 0.08}px)`} y={`calc(0% + ${contextApp.unitpx * 0.08}px)`} />
-          </path>
-          <path lineTo>
-            <path x={`calc(0% + ${contextApp.unitpx * 0}px)`} y='0%' />
-          </path>
+      <path fill fillStyle='rgb(25, 25, 25)' container>
+        <path moveTo>
+          <path x={animationCountPoint.cornerLT[0].x.animationCount} y={animationCountPoint.cornerLT[0].y.animationCount} />
         </path>
+        <path lineTo>
+          <path x={animationCountPoint.cornerRT[0].x.animationCount} y={animationCountPoint.cornerRT[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.cornerRB[0].x.animationCount} y={animationCountPoint.cornerRB[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.cornerLB[0].x.animationCount} y={animationCountPoint.cornerLB[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.cornerLT[0].x.animationCount} y={animationCountPoint.cornerLT[0].y.animationCount} />
+        </path>
+      </path>
 
-        <path fill fillStyle='rgb(25, 65, 65)' container>
+      <path fill fillStyle='rgb(45, 45, 45)' container>
+        <path moveTo>
+          <path x={animationCountPoint.cornerLT[0].x.animationCount} y={animationCountPoint.cornerLT[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.cornerRT[0].x.animationCount} y={animationCountPoint.cornerRT[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.shelfRB[0].x.animationCount} y={animationCountPoint.shelfRB[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.shelfLB[0].x.animationCount} y={animationCountPoint.shelfLB[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.cornerLT[0].x.animationCount} y={animationCountPoint.cornerLT[0].y.animationCount} />
+        </path>
+      </path>
+
+      <path fill fillStyle='rgb(15, 15, 15)' container>
+        <path moveTo>
+          <path x={animationCountPoint.shelfLT[0].x.animationCount} y={animationCountPoint.shelfLT[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.shelfRT[0].x.animationCount} y={animationCountPoint.shelfRT[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.shelfRB[0].x.animationCount} y={animationCountPoint.shelfRB[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.shelfLB[0].x.animationCount} y={animationCountPoint.shelfLB[0].y.animationCount} />
+        </path>
+        <path lineTo>
+          <path x={animationCountPoint.shelfLT[0].x.animationCount} y={animationCountPoint.shelfLT[0].y.animationCount} />
+        </path>
+      </path>
+
+      {/* <path fill fillStyle='rgb(25, 65, 65)' container>
           <path moveTo>
             <path x={`calc(100% + ${contextApp.unitpx * 0}px)`} y='0%' />
           </path>
@@ -131,24 +163,25 @@ function ComponentInWar(props) {
           <path lineTo>
             <path x={`calc(100% + ${contextApp.unitpx * 0}px)`} y='0%' />
           </path>
-        </path>
+        </path> */}
 
-        {
-          new Array(4).fill().map((i,index) => {
-            return <ComponentRivet cx={fixed.rivet[index].x} cy={fixed.rivet[index].y}/>
+      {/* {
+          new Array(4).fill().map((i, index) => {
+            return <ComponentRivet cx={animationCountPoint.rivet[index].x.animationCount} cy={animationCountPoint.rivet[index].y.animationCount} />
           })
-        }
+        } */}
 
-        {
-          new Array(7).fill().map((i,index) => {
-            return <ComponentPlank cx={fixed.plank[index].x} cy={fixed.plank[index].y}/>
-          })
-        }
+      {
+        new Array(7).fill().map((i, index) => {
+          const onPointerDown = (e) => {
+            e.stopPropagation()
+            animationCountPoint.plank[index].y.setAnimationCount(i => i + contextApp.unitpx * 0.04)
+          }
+          return <ComponentPlank cx={animationCountPoint.plank[index].x.animationCount} cy={animationCountPoint.plank[index].y.animationCount} onPointerDown={onPointerDown} />
+        })
+      }
 
-      </layout>
-
-      <rect onPointerDown={onPointerDown} onPointerUp={onPointerUp} onPointerUpAway={onPointerUp} />
-    </>
+    </layout>
 
   return Component
 }
