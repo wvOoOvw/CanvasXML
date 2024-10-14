a68be6dc6e7a17dceb8d1a41e5b1af5eaf718509
xi.chenchao <xt02755@MarkdeMacBook-Pro.local>
2024-10-14 19:30:46 +0800

    x

--- package/Canvas2d/Tag.Component.Arc.js
+++ package/Canvas2d/Tag.Component.Arc.js
@@ -7,8 +7,8 @@ const App = {
   },
 
   onLocationMount: (dom) => {
-    if (dom.props.w === undefined && dom.props.radius) dom.props.w = dom.props.radius * 2
-    if (dom.props.h === undefined && dom.props.radius) dom.props.h = dom.props.radius * 2
+    if (dom.props.w === undefined && dom.props.radius !== undefined) dom.props.w = dom.props.radius * 2
+    if (dom.props.h === undefined && dom.props.radius !== undefined) dom.props.h = dom.props.radius * 2
   },
 
   onRenderMount: (dom) => {
